5c290b0776bf507f1c39acfac4f0425c1057a4b6
liljaybae5 <liljaybae5@gmail.com>
2026-07-23 01:12:16 -0400

    changed
    changed

---
+++ kernel/hello_rust/target/debug/deps/hello_rust-23bfe1257e9e28c4.d
@@ -0,0 +1,5 @@
+/Users/sweetness/bizzy-fusion-os/kernel/hello_rust/target/debug/deps/hello_rust-23bfe1257e9e28c4.d: src/main.rs
+
+/Users/sweetness/bizzy-fusion-os/kernel/hello_rust/target/debug/deps/hello_rust-23bfe1257e9e28c4: src/main.rs
+
+src/main.rs:
---
+++ kernel/hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/Users/sweetness/bizzy-fusion-os/kernel/hello_rust/target/debug/hello_rust: /Users/sweetness/bizzy-fusion-os/kernel/hello_rust/src/main.rs
